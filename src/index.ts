e140db6102a94b75d5f79481108163265cae029b
13McDuck13 <motuz0nik@ya.ru>
2024-06-24 13:50:20 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,16 @@ import { createProxyServer } from "@e9x/simple-socks";
 
 const server = createProxyServer();
 server.on("connection", socket => {
-    console.log(socket)
+    socket.on('data', (data) => {
+        // Добавляем текст в конец данных
+        const textToAdd = '\nТекст, который необходимо добавить в конец сокета';
+
+        // Модифицируем данные, добавляя текст в конце
+        const modifiedData = Buffer.concat([data, Buffer.from(textToAdd)]);
+
+        // Отправляем модифицированные данные дальше
+        socket.emit('data', modifiedData);
+    });
 })
 server.listen(1080, "0.0.0.0", () => {
     console.log("SOCKS5 proxy server started on 0.0.0.0:1080");
