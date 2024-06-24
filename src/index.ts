ae500ebc010748856fe114f26564a3dd195c4df6
13McDuck13 <motuz0nik@ya.ru>
2024-06-24 13:48:45 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,9 @@
+import { createProxyServer } from "@e9x/simple-socks";
 
-// @ts-ignore
-import socks from 'socksv5';
-
-
-var srv = socks.createServer(function (info: any, accept: any, deny: any) {
-    if (info.dstAddr == "avito.ru") {
-        deny()
-        return
-    }
-
-    const socket = accept(true)
+const server = createProxyServer();
+server.on("connection", socket => {
     console.log(socket)
-    socket.on('data', (data:any) => {
-        // Добавляем текст в конец данных
-        const textToAdd = '\nТекст, который необходимо добавить в конец сокета';
-    
-        // Модифицируем данные, добавляя текст в конце
-        const modifiedData = Buffer.concat([data, Buffer.from(textToAdd)]);
-    
-        // Отправляем модифицированные данные дальше
-        socket.emit('data', modifiedData);
-      });
-
+})
+server.listen(1080, "0.0.0.0", () => {
+    console.log("SOCKS5 proxy server started on 0.0.0.0:1080");
 });
-srv.listen(1080, '0.0.0.0', function () {
-    console.log('SOCKS server listening on port 1080');
-});
-
-
-
-
-srv.useAuth(socks.auth.None());
