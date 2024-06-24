577d62e4691e01096eb3478d2c7891141351a560
13McDuck13 <motuz0nik@yandex.ru>
2024-06-24 13:18:46 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
 
 // @ts-ignore
 import socks from 'socksv5';
- 
 
-var srv = socks.createServer(function(info:any, accept:any, deny:any) {
+
+var srv = socks.createServer(function (info: any, accept: any, deny: any) {
     console.log(info)
+    if (info.dstAddr == "avito.ru") {
+        deny()
+        return
+    }
+    var socket;
+    if (socket = accept(true)) {
+        var body = 'Hello ' + info.srcAddr + '!\n\nToday is: ' + (new Date());
+        socket.end([
+            'HTTP/1.1 200 OK',
+            'Connection: close',
+            'Content-Type: text/plain',
+            'Content-Length: ' + Buffer.byteLength(body),
+            '',
+            body
+        ].join('\r\n'));
+    }
     accept();
-  });
-  srv.listen(1080, '0.0.0.0', function() {
+
+});
+srv.listen(1080, '0.0.0.0', function () {
     console.log('SOCKS server listening on port 1080');
-  });
-   
-  srv.useAuth(socks.auth.None());
+});
+
+srv.useAuth(socks.auth.None());
