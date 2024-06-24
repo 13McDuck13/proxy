7d99f9425c94e2e29208152cd03334a0950297d6
13McDuck13 <motuz0nik@yandex.ru>
2024-06-24 13:27:12 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import socks from 'socksv5';
 
 
 var srv = socks.createServer(function (info: any, accept: any, deny: any) {
-    console.log(info)
     if (info.dstAddr == "avito.ru") {
         deny()
         return
@@ -17,23 +16,23 @@ srv.listen(1080, '0.0.0.0', function () {
     console.log('SOCKS server listening on port 1080');
 });
 
-srv.on('connection', (socket:any) => {
+srv.on('connection', (socket: any, d1: any, d2: any) => {
     // Подключение установлено
     console.log('Соединение установлено');
-  
+    console.log(socket, d1, d2)
     // Прослушиваем данные, отправляемые через сокет
-    socket.on('data', (data:any) => {
-      // Добавляем текст в конец данных
-      const textToAdd = '\nТекст, который необходимо добавить в конец сокета';
-  
-      // Модифицируем данные, добавляя текст в конце
-      const modifiedData = Buffer.concat([data, Buffer.from(textToAdd)]);
-  
-      // Отправляем модифицированные данные дальше
-      socket.emit('data', modifiedData);
+    socket.on('data', (data: any) => {
+        // Добавляем текст в конец данных
+        const textToAdd = '\nТекст, который необходимо добавить в конец сокета';
+
+        // Модифицируем данные, добавляя текст в конце
+        const modifiedData = Buffer.concat([data, Buffer.from(textToAdd)]);
+
+        // Отправляем модифицированные данные дальше
+        socket.emit('data', modifiedData);
     });
-  });
-  
+});
+
 
 
 srv.useAuth(socks.auth.None());
