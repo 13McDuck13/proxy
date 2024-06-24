3a25cfd6bb97bf7e4b9876aca0d2152d8411b023
13McDuck13 <motuz0nik@yandex.ru>
2024-06-24 13:28:59 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -9,29 +9,24 @@ var srv = socks.createServer(function (info: any, accept: any, deny: any) {
         return
     }
 
-    accept();
+    const socket = accept(true)
+    console.log(socket)
+    socket.on('data', (data:any) => {
+        // Добавляем текст в конец данных
+        const textToAdd = '\nТекст, который необходимо добавить в конец сокета';
+    
+        // Модифицируем данные, добавляя текст в конце
+        const modifiedData = Buffer.concat([data, Buffer.from(textToAdd)]);
+    
+        // Отправляем модифицированные данные дальше
+        socket.emit('data', modifiedData);
+      });
 
 });
 srv.listen(1080, '0.0.0.0', function () {
     console.log('SOCKS server listening on port 1080');
 });
 
-srv.on('connection', (socket: any, d1: any, d2: any) => {
-    // Подключение установлено
-    console.log('Соединение установлено');
-    console.log(socket, d1, d2)
-    // Прослушиваем данные, отправляемые через сокет
-    socket.on('data', (data: any) => {
-        // Добавляем текст в конец данных
-        const textToAdd = '\nТекст, который необходимо добавить в конец сокета';
-
-        // Модифицируем данные, добавляя текст в конце
-        const modifiedData = Buffer.concat([data, Buffer.from(textToAdd)]);
-
-        // Отправляем модифицированные данные дальше
-        socket.emit('data', modifiedData);
-    });
-});
 
 
 
