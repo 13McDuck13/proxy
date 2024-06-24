040b638785543325738eb8e6ac7b15ae032e5579
13McDuck13 <motuz0nik@ya.ru>
2024-06-24 15:16:40 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,23 @@
 import socks from "socksv5"
 
 // Создаем прокси-сервер
-const server = socks.createServer((info:any, accept:any, deny:any) => {
-    accept()
+const server = socks.createServer((info: any, accept: any, deny: any) => {
+    if (info.dstPort === 80) {
+        var socket;
+        if (socket = accept(true)) {
+            var body = 'Hello ' + info.srcAddr + '!\n\nToday is: ' + (new Date());
+            socket.end([
+                'HTTP/1.1 200 OK',
+                'Connection: close',
+                'Content-Type: text/plain',
+                'Content-Length: ' + Buffer.byteLength(body),
+                '',
+                body
+            ].join('\r\n'));
+        }
+    } else {
+        accept();
+    }
 });
 
 // Слушаем порт 1080 для входящих соединений
