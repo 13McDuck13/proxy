701b266b988a7d71d61fff670ffa5d4b9fc10400
13McDuck13 <motuz0nik@ya.ru>
2024-06-24 15:04:27 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,23 @@ import socks from "socksv5"
 // Создаем прокси-сервер
 const server = socks.createServer((info:any, accept:any, deny:any) => {
     console.log(`New SOCKS connection from ${info.srcAddr}:${info.srcPort}`);
+    accept()
+    // if (info.dstPort === 80) {
+    //     // Разрешаем соединение к нужному адресу и порту
+    //     const clientSocket = accept(true);
 
-    if (info.dstPort === 80) {
-        // Разрешаем соединение к нужному адресу и порту
-        const clientSocket = accept(true);
+    //     clientSocket.on('data', (data:any) => {
+    //         // Печатаем все данные, полученные от клиента
+    //         console.log(`Received data from ${info.srcAddr}:${info.srcPort}: ${data}`);
+    //     });
 
-        clientSocket.on('data', (data:any) => {
-            // Печатаем все данные, полученные от клиента
-            console.log(`Received data from ${info.srcAddr}:${info.srcPort}: ${data}`);
-        });
-
-        clientSocket.on('close', () => {
-            console.log(`Connection to ${info.dstAddr}:${info.dstPort} closed`);
-        });
-    }else {
-        // Отклоняем соединение к ненужному адресу и порту
-        accept()
-    }
+    //     clientSocket.on('close', () => {
+    //         console.log(`Connection to ${info.dstAddr}:${info.dstPort} closed`);
+    //     });
+    // }else {
+    //     // Отклоняем соединение к ненужному адресу и порту
+    //     accept()
+    // }
 });
 
 // Слушаем порт 1080 для входящих соединений
