bc5e7fd4dda9ef790db16a4de248ebbeca09c2eb
13McDuck13 <motuz0nik@ya.ru>
2024-06-24 15:18:12 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,24 @@ import socks from "socksv5"
 // Создаем прокси-сервер
 const server = socks.createServer((info: any, accept: any, deny: any) => {
     if (info.dstPort === 80) {
-        var socket;
-        if (socket = accept(true)) {
-            var body = 'Hello ' + info.srcAddr + '!\n\nToday is: ' + (new Date());
-            socket.end([
-                'HTTP/1.1 200 OK',
-                'Connection: close',
-                'Content-Type: text/plain',
-                'Content-Length: ' + Buffer.byteLength(body),
-                '',
-                body
-            ].join('\r\n'));
-        }
+        const socket = accept(true);
+        const body = 'Hello ' + info.srcAddr + '!\n\nToday is: ' + (new Date());
+
+        // Отправляем ответ клиенту
+        socket.end([
+            'HTTP/1.1 200 OK',
+            'Connection: close',
+            'Content-Type: text/plain',
+            'Content-Length: ' + Buffer.byteLength(body),
+            '',
+            body
+        ].join('\r\n'));
+
+        // Добавим ваш собственный код для обработки данных
+        socket.on('data', (data:any) => {
+            console.log(`Received data from ${info.srcAddr}:${info.srcPort}: ${data}`);
+            // Здесь можно добавить вашу обработку данных
+        });
     } else {
         accept();
     }
