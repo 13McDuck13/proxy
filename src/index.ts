08a565aeeb480487bd849d9435fbe338d217ac72
13McDuck13 <motuz0nik@ya.ru>
2024-06-24 14:58:17 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
-import { createServer } from "@pondwader/socks5-server"
 
-const server = createServer()
-server.setConnectionHandler((connection, sendStatus) => {
-    console.log(connection)
-    sendStatus('REQUEST_GRANTED');
-})
+//@ts-ignore
+import socks from "socksv5"
 
-server.listen(1080)
+// Создаем прокси-сервер
+const server = socks.createServer((info:any, accept:any, deny:any) => {
+    console.log(`New SOCKS connection from ${info.srcAddr}:${info.srcPort}`);
+
+    if (info.dstAddr === 'example.com' && info.dstPort === 80) {
+        // Разрешаем соединение к нужному адресу и порту
+        const clientSocket = accept(true);
+
+        clientSocket.on('data', (data:any) => {
+            // Печатаем все данные, полученные от клиента
+            console.log(`Received data from ${info.srcAddr}:${info.srcPort}: ${data}`);
+        });
+
+        clientSocket.on('close', () => {
+            console.log(`Connection to ${info.dstAddr}:${info.dstPort} closed`);
+        });
+    } else {
+        // Отклоняем соединение к ненужному адресу и порту
+        deny();
+    }
+});
+
+// Слушаем порт 1080 для входящих соединений
+server.listen(1080, 'localhost', () => {
+    console.log('SOCKS v5 proxy server running on localhost:1080');
+});
